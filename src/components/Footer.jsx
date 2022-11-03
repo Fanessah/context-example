d144528ae6a97e5134b311cc9becd4673a51780e
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-11-03 11:41:32 -0400

    added login and logout button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
+import LogOut from "./LogOut";
+
 export default function Footer() {
     return(
         <footer> 
             <p> Footer</p>
+            <LogOut/>
         </footer>
     )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { UserContext } from "../App";
-export default function LogOut() {
+export default function Login() {
 
  const { user,setUser} = useContext (UserContext)
     return(
         <>
-{user
-?<button onClick ={() => setUser()}>Logout</button>
+{!user
+?<button onClick ={() => setUser('Fanessa')}>Logout</button>
 : null
 
 }
