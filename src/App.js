8e9d274f8d0ef0a15e823d9ae31624105a325ecd
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-11-03 11:19:26 -0400

    created the state variable(user) which is what i want to share with all of the components, wrapped all children <header>,<main>. Menu bar and title is inside header and welcome/footer are inside Main. By wrapping the 2 larger components everybody gets wrapped .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-
-import './App.css';
+import {useState,createContext} from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import './App.css';
 
+export const UserContext = createContext(null);
 
 function App() {
+  const[user,setUser] = useState('Fanessa');
   return (
-    <>
+    <UserContext.Provider value={{user,setUser}}>
+      
     <Header/>
     <Main/>
   
-    </>
+    </UserContext.Provider>
   );
 }
 
