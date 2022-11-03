5ff84047ff75cafd8a227e0060d2097196fc43ac
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-11-03 11:24:05 -0400

    updated so user is at the top of welcome but set user is not there because we do n ot need to chnage the user and thats why we need set

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { UserContext } from "../App";
+
 export default function Welcome() {
+    const { user} = useContext(UserContext)
     return(
         <section> 
             <h2> Welcome</h2>
